ddb7349e78fe3fd2522a17f35f1a03fc8136c378
Gauransh Bhatnagar <gauranshbhatnagar125@gmail.com>
2026-09-16 23:49:19 +0530

    fix: use next/link instead of a tags

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="border-b p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="font-bold text-xl">Company Store</div>
         <div className="space-x-4">
-          <a href="/products" className="hover:underline">Products</a>
-          <a href="/solutions" className="hover:underline">Solutions</a>
-          <a href="/resources" className="hover:underline">Resources</a>
-          <a href="/cart" className="hover:underline">Cart</a>
+          <Link href="/products" className="hover:underline">Products</Link>
+          <Link href="/solutions" className="hover:underline">Solutions</Link>
+          <Link href="/resources" className="hover:underline">Resources</Link>
+          <Link href="/cart" className="hover:underline">Cart</Link>
         </div>
       </div>
     </nav>
